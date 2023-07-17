478c5c77543d369c480914d3538bdbdd901d91e7
mizan21 <mizanorrahman398@gmail.com>
2023-07-17 15:42:11 +0600

    Some code changes

--- src/Components/OtherPages/BorrowBooks/BorrowBooks.jsx
+++ src/Components/OtherPages/BorrowBooks/BorrowBooks.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import BorrowBook from "./BorrowBook";
 
 const BorrowBooks = () => {
   const borrowBooks = useLoaderData();
